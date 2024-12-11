0b62ae1febdc7786478620e9ee815112d2392909
tebogo60 <tjjmalebana@gmail.com>
2024-12-11 20:03:47 +0200

    updated - now using functional development

--- buildscript.sh
+++ buildscript.sh
@@ -3,44 +3,114 @@
 # basic run cmd = ./buildscript.sh reponame -i -r
 
 # checking if git is installed
-if command -v git > /dev/null; then
-  echo "Git is installed: $(git --version)"
-else
-  sudo apt install git
-fi
-
-CMD_LIST=($1 $2 $3)
-
-# preparing cmds
-echo "Preparing commands..."
-if [ ${#CMD_LIST[@]} -eq 0 ]; then
-  echo " > Please enter a valid command > ./buildscript.sh reponame -i -r"
-else
-  if [ ${#1} -lt 3 ]; then
-    echo "  > Repo name must be greater than 4 characters"
+check_git_initialization() {
+  if command -v git > /dev/null; then
+    echo "Git is installed: $(git --version)"
   else
-    echo "  > Creating $1 repo..."
-    mkdir $1/
-    echo "  > $1 repo Created!"
-    if [ "$2" = "-i" ]; then
-      echo "  > Creating .gitignore file..."
-      touch $1/.gitignore
-      echo "  > .gitignore > Created!"
-    fi
-    if [ "$3" = "-r" ]; then
-      echo "  > Creating README.md file..."
-      echo "# $1" > $1/README.md
-      echo "  > README.md > Created!
-"
-    fi
-
-    cd $1
-    git init
-    git add . 
-    git commit -m "Initial Commit"
-
-    echo "
-  > Git repository initialized and committed!"
+    echo "Git is not installed. Installing Git..."
+    sudo apt install git -y
   fi
-fi
+}
 
+# get repo name
+get_repo_name() {
+  while true; do
+    echo -n "Enter repository name: "
+    read repo_name
+
+    if [ ${#repo_name} -lt 3 ]; then
+      echo "Repository name must be at least 3 characters long. Please try again."
+    else
+      echo "Creating directory $repo_name..."
+      mkdir -p "$repo_name"
+      break
+    fi
+  done
+}
+
+# adding a readme file
+add_readme() {
+  while true; do
+    echo -n "Add a README.md file? (y/n): "
+    read readme
+
+    if [ "$readme" = "y" ]; then
+      echo "Creating README.md file..."
+      echo "# $repo_name" > "$repo_name/README.md"
+      echo "README.md created successfully!"
+      break
+    elif [ "$readme" = "n" ]; then
+      echo "Skipping README.md creation."
+      break
+    else
+      echo "Invalid input. Please enter 'y' or 'n'."
+    fi
+  done
+}
+
+# adding a gitignore file
+add_gitignore() {
+  while true; do
+    echo -n "Add a .gitignore file? (y/n): "
+    read gitignore
+
+    if [ "$gitignore" = "y" ]; then
+      echo "Creating .gitignore file..."
+      touch "$repo_name/.gitignore"
+      echo ".gitignore created successfully!"
+      break
+    elif [ "$gitignore" = "n" ]; then
+      echo "Skipping .gitignore creation."
+      break
+    else
+      echo "Invalid input. Please enter 'y' or 'n'."
+    fi
+  done
+}
+
+# setting up git for the repo
+setup_git() {
+  echo "Initializing Git repository..."
+  cd "$repo_name"
+  git init
+  git add .
+  git commit -m "Initial Commit"
+  echo "Git repository initialized and files committed!"
+
+  # while true; do
+  #   echo -n "Do you want to push the repository to GitHub? (y/n): "
+  #   read push_repo
+
+  #   if [ "$push_repo" = "y" ]; then
+  #     echo -n "Enter your GitHub username: "
+  #     read username
+
+  #     echo "> Adding remote origin..."
+  #     git remote add origin git@github.com:$username/$repo_name.git
+  #     echo "> Pushing repository to GitHub..."
+  #     git branch -M main
+  #     git push -u origin main
+  #     echo "Repository pushed successfully!"
+  #     break
+  #   elif [ "$push_repo" = "n" ]; then
+  #     echo "Skipping push to GitHub."
+  #     break
+  #   else
+  #     echo "Invalid input. Please enter 'y' or 'n'."
+  #   fi
+  # done
+}
+
+# run all function
+run() {
+  check_git_installed
+  get_repo_name
+  add_readme
+  add_gitignore
+  setup_git
+
+  echo "Repository setup complete!"
+}
+
+# run the script
+run
